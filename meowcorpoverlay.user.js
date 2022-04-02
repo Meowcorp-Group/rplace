08616c8aa713bf597bc9ac4ee5790a15aa54e354
Waoweens <30313089+Waoweens@users.noreply.github.com>
2022-04-03 06:44:46 +0700

    update overlay script to use another file host

--- meowcorpoverlay.user.js
+++ meowcorpoverlay.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/Meowcorp-Group/rplace/main/template.png";
+            i.src = "https://firebasestorage.googleapis.com/v0/b/meowcorp-redirect.appspot.com/o/template.png?alt=media";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
